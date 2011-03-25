e5d2b397ab585f960e01355b59b30618656216a0
Chase Southard <chase.southard@gmail.com>
2011-03-25 13:02:25 -0400

    changing time to mins for clarity

--- lib/graceful_rounding.rb
+++ lib/graceful_rounding.rb
@@ -1,34 +1,34 @@
 module GracefulRounding
-  class Time
-    def self.graceful_rounding
-      time = self.min
+  class mins
+    def graceful_rounding
+      mins = self.min
 
-      if time == 00
+      if mins == 00
         return
-      elsif (1..7).cover?(time)
-        return self -= (time*60)
-      elsif (8..14).cover?(time)
-        return self += (15 - time)*60
-      elsif time == 15
+      elsif (1..7).cover?(mins)
+        return self -= (mins*60)
+      elsif (8..14).cover?(mins)
+        return self += (15 - mins)*60
+      elsif mins == 15
         return
-      elsif (16..22).cover?(time)
-        return self -= (time -15)*60
-      elsif (23..29).cover?(time)
-        return self += (30 - time)*60
-      elsif time == 30
+      elsif (16..22).cover?(mins)
+        return self -= (mins -15)*60
+      elsif (23..29).cover?(mins)
+        return self += (30 - mins)*60
+      elsif mins == 30
         return
-      elsif (31..37).cover?(time)
-        return self -= (time - 30)*60
-      elsif (38..44).cover?(time)
-        return self += (45 - time)*60
-      elsif time == 45
+      elsif (31..37).cover?(mins)
+        return self -= (mins - 30)*60
+      elsif (38..44).cover?(mins)
+        return self += (45 - mins)*60
+      elsif mins == 45
         return
-      elsif (46..52).cover?(time)
-        return self -= (time - 45)*60
-      elsif (53..59).cover?(time)
-        return self += (60 - time)*60
+      elsif (46..52).cover?(mins)
+        return self -= (mins - 45)*60
+      elsif (53..59).cover?(mins)
+        return self += (60 - mins)*60
       else
-        raise 'value of time.min not between 00..59'
+        raise 'value of Time.min is not between 00..59'
       end
       
     end
