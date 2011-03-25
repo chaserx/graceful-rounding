1a790f033379965f64ccce74880aec0cd15be831
Chase Southard <chase.southard@gmail.com>
2011-03-25 19:52:48 -0400

    properly mixing in graceful_rounding method into Time

--- lib/graceful_rounding.rb
+++ lib/graceful_rounding.rb
@@ -1,36 +1,41 @@
 module GracefulRounding
-  class mins
+
     def graceful_rounding
-      mins = self.min
+      @time = self
+      mins = @time.min
 
       if mins == 00
         return
       elsif (1..7).cover?(mins)
-        return self -= (mins*60)
+        return @time -= (mins*60)
       elsif (8..14).cover?(mins)
-        return self += (15 - mins)*60
+        return @time += (15 - mins)*60
       elsif mins == 15
         return
       elsif (16..22).cover?(mins)
-        return self -= (mins -15)*60
+        return @time -= (mins - 15)*60
       elsif (23..29).cover?(mins)
-        return self += (30 - mins)*60
+        return @time += (30 - mins)*60
       elsif mins == 30
         return
       elsif (31..37).cover?(mins)
-        return self -= (mins - 30)*60
+        return @time -= (mins - 30)*60
       elsif (38..44).cover?(mins)
-        return self += (45 - mins)*60
+        return @time += (45 - mins)*60
       elsif mins == 45
         return
       elsif (46..52).cover?(mins)
-        return self -= (mins - 45)*60
+        return @time -= (mins - 45)*60
       elsif (53..59).cover?(mins)
-        return self += (60 - mins)*60
+        return @time += (60 - mins)*60
       else
         raise 'value of Time.min is not between 00..59'
       end
       
     end
-  end
+
+end
+
+class Time
+  include GracefulRounding
 end
